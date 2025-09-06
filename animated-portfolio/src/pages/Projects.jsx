b228fef7aaf07b3ebee286b4832c558accc7288f
Amisha Abasana <abasanaamisha2001@gmail.com>
2025-09-06 23:57:31 +0530

    Update Projects page layout and styling
    Enhanced the Projects page with padding, background color, and a styled heading. Prepared structure for future content additions.

--- animated-portfolio/src/pages/Projects.jsx
+++ animated-portfolio/src/pages/Projects.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <div className="p-4">
+      <div className="pt-[40vh] bg-red-400">
+        <h2 className="font-[font2] text-[9vw] uppercase">Projets<sup>16</sup></h2>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
