15d9af8d3f533f252b32c864c98376ccb1ae0cff
Amisha Abasana <abasanaamisha2001@gmail.com>
2025-09-08 00:25:45 +0530

    Add ProjectCard component and integrate into Projects page
    Introduced a new ProjectCard component to display project images with hover effects. Updated the Projects page to import and render ProjectCard, preparing for dynamic project listing.

--- animated-portfolio/src/pages/Projects.jsx
+++ animated-portfolio/src/pages/Projects.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import ProjectCard from "../components/projects/ProjectCard";
 
 const Projects = () => {
+  const projects = [{
+    image1: "https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg",
+    image2: "https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg"
+  }]
   return (
     <div className="p-4">
-      <div className="pt-[40vh] bg-red-400">
-        <h2 className="font-[font2] text-[9vw] uppercase">Projets<sup>16</sup></h2>
+      <div className="pt-[40vh]">
+        <h2 className="font-[font2] text-[9vw] uppercase">
+          Projets<sup>16</sup>
+        </h2>
       </div>
-      <div>
-        
+      <div className="-mt-10">
+        {projects.map(<ProjectCard/>)}
       </div>
     </div>
   );
