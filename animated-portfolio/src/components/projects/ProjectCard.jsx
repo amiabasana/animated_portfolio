bb83bfc3413eecc544127043ed8341fe1ee9ad28
Amisha Abasana <abasanaamisha2001@gmail.com>
2025-09-10 23:32:17 +0530

    made ProjectCard component and Projects component
    made ProjectCard component and Projects component:
    passed props and use some gsap

--- animated-portfolio/src/components/projects/ProjectCard.jsx
+++ animated-portfolio/src/components/projects/ProjectCard.jsx
@@ -1,38 +1,35 @@
-import React from 'react'
+import React from "react";
 
-const ProjectCard = () => {
+const ProjectCard = (props) => {
   return (
-    <div>
-      <div className="w-full h-[800px] mb-4 flex gap-4">
-          <div className="group w-1/2 h-full transition-all relative rounded-none hover:rounded-[50px] overflow-hidden">
-            <img
-              className="w-full h-full object-cover"
-              src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg"
-              alt="image 1"
-            />
-            <div className="w-full h-full top-0 left-0 absolute bg-black/10 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-all">
-              <h2 className="uppercase text-6xl font-[font1] border-2 pt-3 px-2 text-white border-white rounded-full">
-                Voir le projet
-              </h2>
-            </div>
-          </div>
-
-           <div className="group w-1/2 h-full transition-all relative rounded-none hover:rounded-[50px] overflow-hidden">
-            <img
-              className="w-full h-full object-cover"
-              src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg"
-              alt="image 1"
-            />
-            <div className="w-full h-full top-0 left-0 absolute bg-black/10 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-all">
-              <h2 className="uppercase text-6xl font-[font1] border-2 pt-3 px-2 text-white border-white rounded-full">
-                Voir le projet
-              </h2>
-            </div>
-          </div>
-          
+    <>
+      <div className="group w-1/2 h-full transition-all relative rounded-none hover:rounded-[50px] overflow-hidden">
+        <img
+          className="w-full h-full object-cover"
+          src={props.image1}
+          alt="image 1"
+        />
+        <div className="w-full h-full top-0 left-0 absolute bg-black/10 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-all">
+          <h2 className="uppercase text-6xl font-[font1] border-2 pt-3 px-2 text-white border-white rounded-full">
+            Voir le projet
+          </h2>
         </div>
-    </div>
-  )
-}
+      </div>
 
-export default ProjectCard
+      <div className="group w-1/2 h-full transition-all relative rounded-none hover:rounded-[50px] overflow-hidden">
+        <img
+          className="w-full h-full object-cover"
+          src={props.image2}
+          alt="image 1"
+        />
+        <div className="w-full h-full top-0 left-0 absolute bg-black/10 flex justify-center items-center opacity-0 group-hover:opacity-100 transition-all">
+          <h2 className="uppercase text-6xl font-[font1] border-2 pt-3 px-2 text-white border-white rounded-full">
+            Voir le projet
+          </h2>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProjectCard;
--- animated-portfolio/src/pages/Projects.jsx
+++ animated-portfolio/src/pages/Projects.jsx
@@ -1,11 +1,37 @@
 import React from "react";
 import ProjectCard from "../components/projects/ProjectCard";
+import useGSAP from '@gsap/react';
+import gsap from "gsap";
 
 const Projects = () => {
-  const projects = [{
-    image1: "https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg",
-    image2: "https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg"
-  }]
+  const projects = [
+    {
+      image1: 'https://k72.ca/uploads/caseStudies/OKA/OKA_thumbnail-1280x960.jpg',
+      image2: 'https://k72.ca/uploads/caseStudies/COUP_FUMANT/CF_thumbnail-1280x960.jpg', 
+    },
+    {
+      image1:'https://k72.ca/uploads/caseStudies/SHELTON/thumbnailimage_shelton-1280x960.jpg',
+      image2: 'https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg'
+    },
+    {
+      image1:'https://k72.ca/uploads/caseStudies/LAMAJEURE_-_Son_sur_mesure/chalaxeur-thumbnail_img-1280x960.jpg',
+      image2:'https://k72.ca/uploads/caseStudies/Opto/thumbnailimage_opto-1280x960.jpg',
+    },
+    {
+      image1: 'https://k72.ca/uploads/caseStudies/PME-MTL/PME-MTL_Thumbnail-1280x960.jpg',
+      image2: 'https://k72.ca/uploads/caseStudies/FRUITE/Fruite_thumbnail_bbq-1280x960.jpg'
+    }];
+
+    gsap.registerPlugin(scrollTrigger)
+
+    useGSAP(function(){
+      gsap.from('.hero',{
+        height: 0,
+        scrollTrigger: {
+            trigger: '.parent-project'
+        }
+      });
+    })
   return (
     <div className="p-4">
       <div className="pt-[40vh]">
@@ -13,8 +39,12 @@ const Projects = () => {
           Projets<sup>16</sup>
         </h2>
       </div>
-      <div className="-mt-10">
-        {projects.map(<ProjectCard/>)}
+      <div className="-mt-10 parent-project">
+        {projects.map(function (element, index){
+          return <div key={index} className="hero w-full h-[800px] mb-4 flex gap-4">
+          <ProjectCard image1={element.image1} image2={element.image2}/>
+          </div>
+        })}
       </div>
     </div>
   );
